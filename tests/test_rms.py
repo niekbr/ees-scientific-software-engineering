039da82015c7926e4e41cf1b1a7739a0262a8f47
Niek Brekelmans <10487997+niekbr@users.noreply.github.com>
2024-12-04 15:37:07 +0100

    style

--- tests/test_rms.py
+++ tests/test_rms.py
@@ -1,7 +1,8 @@
+import json
+from pathlib import Path
+
 import numpy as np
 import pytest
-from pathlib import Path
-import json
 
 from ees_scientific_software_engineering.rms import rms
 
@@ -10,6 +11,7 @@ DATA_PATH = Path(__file__).parent / "data"
 
 def test_rms():
     assert np.isclose(rms(np.array([4.0, 1.0, 8.0])), 5.1962)
+
 
 def test_rms_from_data():
     with open(DATA_PATH / "test_rms.json") as f:
