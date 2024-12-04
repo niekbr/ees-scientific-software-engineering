a1719529485921f230dd58595eb53c945f78fbeb
ballard asare-bediako <b.asarebediako@tue.nl>
2024-12-04 15:10:25 +0100

    added test
    Signed-off-by: ballard asare-bediako <b.asarebediako@tue.nl>

--- src/ees_scientific_software_engineering/rms.py
+++ src/ees_scientific_software_engineering/rms.py
@@ -1,13 +1,11 @@
 """A module that calculates the root mean square
 """
 
-import numpy as np 
+import numpy as np
 
 
 def rms(input_array: np.ndarray) -> float:
-    input_array_squared = input_array ** 2
+    input_array_squared = input_array**2
     rms = np.sqrt(np.mean(input_array_squared))
 
     return rms
-
- 
