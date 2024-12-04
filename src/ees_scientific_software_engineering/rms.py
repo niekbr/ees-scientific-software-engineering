8c1ba5fc8188fa670c17f5ce60cd8fdd936977fa
Niek Brekelmans <10487997+niekbr@users.noreply.github.com>
2024-12-04 15:15:51 +0100

    pylint fixed

--- src/ees_scientific_software_engineering/rms.py
+++ src/ees_scientific_software_engineering/rms.py
@@ -6,6 +6,4 @@ import numpy as np
 
 def rms(input_array: np.ndarray) -> float:
     input_array_squared = input_array**2
-    rms = np.sqrt(np.mean(input_array_squared))
-
-    return rms
+    return np.sqrt(np.mean(input_array_squared))
