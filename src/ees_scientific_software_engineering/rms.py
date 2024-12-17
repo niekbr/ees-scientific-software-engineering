4c19db084a9428e228b13c369dc3e44718a7f7c1
Niek Brekelmans <10487997+niekbr@users.noreply.github.com>
2024-12-17 10:32:46 +0100

    add check for type exception

--- src/ees_scientific_software_engineering/rms.py
+++ src/ees_scientific_software_engineering/rms.py
@@ -5,5 +5,8 @@ import numpy as np
 
 
 def rms(input_array: np.ndarray) -> float:
+    if not isinstance(input_array, np.ndarray):
+        raise TypeError("Argument should be a numpy array!")
+
     input_array_squared = input_array**2
     return np.sqrt(np.mean(input_array_squared))
