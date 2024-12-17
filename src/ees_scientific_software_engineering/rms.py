f6c6d646fb07d65c7800d0117263643027f1ab10
ballard asare-bediako <b.asarebediako@tue.nl>
2024-12-17 11:09:10 +0100

    documentation added
    Signed-off-by: ballard asare-bediako <b.asarebediako@tue.nl>

--- src/ees_scientific_software_engineering/rms.py
+++ src/ees_scientific_software_engineering/rms.py
@@ -4,9 +4,18 @@
 import numpy as np
 
 
-def rms(input_array: np.ndarray) -> float:
+def rms(input_array: np.ndarray) -> float: 
+    """calculating rms
+
+    Args:
+        input_array: numpy array containing floats
+
+    Returns:
+        rms of numbers in array
+    """
     if not isinstance(input_array, np.ndarray):
         raise TypeError("Argument should be a numpy array!")
 
     input_array_squared = input_array**2
     return np.sqrt(np.mean(input_array_squared))
+
